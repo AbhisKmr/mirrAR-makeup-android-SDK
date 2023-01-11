ab0fcc6f799ce1b2bb324bbb3c5dc728bf313048
AbhisKmr <akaks09@gmail.com>
2023-01-11 11:18:45 +0530

    update: added sample project

---
+++ sample/app/src/main/java/com/mirrar/testmakeupsdk/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.mirrar.testmakeupsdk
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.mirrar.makeupsdk.MakeUpFragment
+import com.mirrar.makeupsdk.`interface`.IMakeupCallback
+
+class MainActivity : AppCompatActivity(), IMakeupCallback {
+
+    private lateinit var makeUpFragment: MakeUpFragment
+    private val PERMS = arrayOf(
+        Manifest.permission.CAMERA
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ActivityCompat.requestPermissions(this, PERMS, 0)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        var allowed = true
+        for (i in permissions.indices) {
+            if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                allowed = false
+                break
+            }
+        }
+
+        if (allowed) {
+            makeUpFragment = MakeUpFragment(this)
+            supportFragmentManager.beginTransaction()
+            supportFragmentManager.beginTransaction().replace(R.id.container, makeUpFragment)
+                .commit()
+        }
+    }
+
+    override fun OnClear() {
+        TODO("Not yet implemented")
+    }
+
+    override fun OnEffectCreated() {
+        TODO("Not yet implemented")
+    }
+
+    override fun OnEffectDisplayed() {
+        TODO("Not yet implemented")
+    }
+
+    override fun OnEffectHidden() {
+        TODO("Not yet implemented")
+    }
+
+    override fun OnEffectRemoved() {
+        TODO("Not yet implemented")
+    }
+
+    override fun OnEffectUpdated() {
+        TODO("Not yet implemented")
+    }
+
+    override fun OnStart() {
+        TODO("Not yet implemented")
+    }
+}
