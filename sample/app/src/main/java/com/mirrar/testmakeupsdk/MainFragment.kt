24c7916329f383994bee8790993fa518bebca294
AbhisKmr <akaks09@gmail.com>
2023-01-17 22:11:07 +0530

    update: makeup sample

--- sample/app/src/main/java/com/mirrar/testmakeupsdk/MainFragment.kt
+++ sample/app/src/main/java/com/mirrar/testmakeupsdk/MainFragment.kt
@@ -10,11 +10,30 @@ import com.mirrar.makeupsdk.helper.AppConstraints
 import com.mirrar.makeupsdk.interface_class.IMakeupCallback
 import com.mirrar.testmakeupsdk.databinding.FragmentMainBinding
 import com.visagetechnologies.makeupsdk.Effect
+import okhttp3.internal.notifyAll
 
 class MainFragment : Fragment(), IMakeupCallback {
 
     private lateinit var binding: FragmentMainBinding
     private lateinit var makeUpFragment: MakeUpFragment
+    private val effectMap = mutableMapOf<String, Effect>()
+
+    private val category = arrayListOf(
+        "EyeshadowLid",
+        "LipstickOneTone",
+        "BlushCheekbone",
+        "Highlighter",
+        "BlushContour",
+        "EyebrowsThin"
+    )
+    private val sku = arrayListOf(
+        "Light5861",
+        "D for Danger5864",
+        "Pale Pink5870",
+        "Peachlite5873",
+        "02 Warm5877",
+        "Medium Brown5879"
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,6 +51,16 @@ class MainFragment : Fragment(), IMakeupCallback {
 
         childFragmentManager.beginTransaction().addToBackStack("makeUpFragment")
             .add(R.id.makeupContainer, makeUpFragment, makeUpFragment.tag).commit()
+
+        binding.apply.setOnClickListener {
+            for (i in 0 until category.size){
+                val cat = category[i]
+                val s = sku[i]
+                makeUpFragment.applyMakeupEffect(cat, s)
+            }
+        }
+
+//        makeUpFragment.removeMakeupEffect(effectMap[category[0]]!!)
     }
 
     override fun OnClear() {
@@ -39,7 +68,7 @@ class MainFragment : Fragment(), IMakeupCallback {
     }
 
     override fun OnEffectCreated(effect: Effect) {
-
+        effectMap[effect.GetName()] = effect
     }
 
     override fun OnEffectDisplayed(effect: Effect) {
@@ -51,7 +80,7 @@ class MainFragment : Fragment(), IMakeupCallback {
     }
 
     override fun OnEffectRemoved(effect: Effect) {
-
+        effectMap.remove(effect.GetName())
     }
 
     override fun OnEffectUpdated(effect: Effect) {
